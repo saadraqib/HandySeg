10ef56c0d199d2c29fda19a8c2c18d22f0a8967f
Saad Raqib Aziz <87097921+saadraqib@users.noreply.github.com>
2024-06-22 17:09:16 +0300

    Create __init__.py
    added __init__.py file

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .line_segmentation import line_segmenation
+from .word_segmentation import WordSegmentation
+from .preprocessing import convert2binary, find_pp, gray, cut_blank_header_footer, first_and_last_line, segmented_lines, mark_horizontal_seam, segmented_words
