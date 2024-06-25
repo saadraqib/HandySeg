e81e987042f6b9c34183da6011476b50654fa20c
saadr <saad.rqaziz@gmail.com>
2024-06-25 22:41:40 +0300

    Added a user guide for the code

--- main.py
+++ main.py
@@ -6,26 +6,43 @@ from HandySeg.word_segmentaiton import WordSegmentation
 import matplotlib.pyplot as plt
 import cv2
 
+# import the HandySeg "pip install HandySeg"
 
-img = cv2.imread(r"C:\Users\saadr\OneDrive\Desktop\files\projects\Datasets\HW_part_IAM_dataset\000_01.png")
+img = cv2.imread(r"000_01.png")
 
-# binary_image = convert2binary(img)
-# hpp = find_pp(binary_image, axis=1)
-#
-line_seg = line_segmenation(img,7, show_seam_paths=True)
 
+# Initialize an object of line_segmentation
+# smoothed_factor: It can be varied if there are misclassified lines
+# show_seam_paths: Return the images with the lowest seam paths between each two lines, which are marked on the image
+line_seg = line_segmenation(img, 7, show_seam_paths=True)
+
+
+# Retrieve the line images
 line_imgs = line_seg.retrieve_line_imgs()
-# for i in range(len(imgs)):
-#     plt.imshow(imgs[i],cmap='gray')
-#     plt.title(f"img index: {i}")
-#     plt.show()
 
-word_seg = WordSegmentation(line_imgs[0],True)
-# word_boundary = word_seg.retrieve_word_imgs()
-# for i in range(len(word_boundary)):
-#     plt.imshow(word_boundary[i],cmap='gray')
-#     plt.title(f"img index: {i}")
-#     plt.show()
+# To view the seams
+line_seg.seams
+
+
+# Display each line image
+for i in range(len(line_imgs)):
+    plt.imshow(line_imgs[i], cmap='gray')
+    plt.title(f"img index: {i}")
+    plt.show()
+
+# Initialize a WordSegmentation object
+word_seg = WordSegmentation(line_imgs[0], True)
+
+# To show the boundaries of each word
+word_seg.x_bounds
+
+# Retrieve and display each word image
+word_boundary = word_seg.retrieve_word_imgs()
+for i in range(len(word_boundary)):
+    plt.imshow(word_boundary[i], cmap='gray')
+    plt.title(f"img index: {i}")
+    plt.show()
+
 
 
 
